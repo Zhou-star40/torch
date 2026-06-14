1771e3d52f30b89c1f42588f37c7c31a5643e966
Zhou-star40 <woailiuzhuohui@gmail.com>
2026-06-14 14:05:20 +0800

    添加读取数据集到输入

--- 线性回归/读取数据集.py
+++ 线性回归/读取数据集.py
@@ -1,16 +1,16 @@
-import torch
-import random
-def data_iter(batch_size, features, labels):
-    num_example = len(features)
-    indices = list(range(num_example))
-    random.shuffle(indices)
-    for i in range(0, num_example, batch_size):
-        batch_size = torch.tensor(
-            indices[i: min(i + batch_size, num_example)])
-        yield features[batch_size], labels[batch_size]
+# import torch
+# import random
+# def data_iter(batch_size, features, labels):
+#     num_example = len(features)
+#     indices = list(range(num_example))
+#     random.shuffle(indices)
+#     for i in range(0, num_example, batch_size):
+#         batch_size = torch.tensor(
+#             indices[i: min(i + batch_size, num_example)])
+#         yield features[batch_size], labels[batch_size]
 
-batch_size = 10 
+# batch_size = 10 
 
-for X, y in data_iter(batch_size, features, labels):
-    print(X, '\n', y)
-    break
+# for X, y in data_iter(batch_size, features, labels):
+#     print(X, '\n', y)
+#     break
